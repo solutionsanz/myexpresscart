45265e8f506f74f315451bc97011d99b0d418b51
SolutionsANZ <solutionsanz2@gmail.com>
2020-01-21 19:33:18 +1100

    Adding experiecne apis connectivity

--- routes/payments/paypal.js
+++ routes/payments/paypal.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const common = require('../../lib/common');
-const { indexOrders } = require('../../lib/indexing');
+const {
+    indexOrders
+} = require('../../lib/indexing');
 const paypal = require('paypal-rest-sdk');
 const router = express.Router();
+
+
+const axios = require('axios')
 
 router.get('/checkout_cancel', (req, res, next) => {
     // return to checkout for adjustment or repayment
@@ -15,18 +20,20 @@ router.get('/checkout_return', (req, res, next) => {
     const paymentId = req.session.paymentId;
     const payerId = req.query.PayerID;
 
-    const details = { payer_id: payerId };
+    const details = {
+        payer_id: payerId
+    };
     paypal.payment.execute(paymentId, details, (error, payment) => {
         let paymentApproved = false;
         let paymentMessage = '';
         let paymentDetails = '';
-        if(error){
+        if (error) {
             paymentApproved = false;
 
-            if(error.response.name === 'PAYMENT_ALREADY_DONE'){
+            if (error.response.name === 'PAYMENT_ALREADY_DONE') {
                 paymentApproved = false;
                 paymentMessage = error.response.message;
-            }else{
+            } else {
                 paymentApproved = false;
                 paymentDetails = error.response.error_description;
             }
@@ -45,59 +52,69 @@ router.get('/checkout_return', (req, res, next) => {
         let paymentStatus = 'Approved';
 
         // fully approved
-        if(payment.state === 'approved'){
+        if (payment.state === 'approved') {
             paymentApproved = true;
             paymentStatus = 'Paid';
             paymentMessage = 'Your payment was successfully completed';
             paymentDetails = '<p><strong>Order ID: </strong>' + paymentOrderId + '</p><p><strong>Transaction ID: </strong>' + payment.id + '</p>';
 
             // clear the cart
-            if(req.session.cart){
+            if (req.session.cart) {
                 common.emptyCart(req, res, 'function');
             }
         }
 
         // failed
-        if(payment.failureReason){
+        if (payment.failureReason) {
             paymentApproved = false;
             paymentMessage = 'Your payment failed - ' + payment.failureReason;
             paymentStatus = 'Declined';
         }
 
         // update the order status
-        db.orders.updateOne({ _id: common.getId(paymentOrderId) }, { $set: { orderStatus: paymentStatus } }, { multi: false }, (err, numReplaced) => {
-            if(err){
+        db.orders.updateOne({
+            _id: common.getId(paymentOrderId)
+        }, {
+            $set: {
+                orderStatus: paymentStatus
+            }
+        }, {
+            multi: false
+        }, (err, numReplaced) => {
+            if (err) {
                 console.info(err.stack);
             }
-            db.orders.findOne({ _id: common.getId(paymentOrderId) }, (err, order) => {
-                if(err){
+            db.orders.findOne({
+                _id: common.getId(paymentOrderId)
+            }, (err, order) => {
+                if (err) {
                     console.info(err.stack);
                 }
 
                 // add to lunr index
                 indexOrders(req.app)
-                .then(() => {
-                    // set the results
-                    req.session.messageType = 'success';
-                    req.session.message = paymentMessage;
-                    req.session.paymentEmailAddr = order.orderEmail;
-                    req.session.paymentApproved = paymentApproved;
-                    req.session.paymentDetails = paymentDetails;
+                    .then(() => {
+                        // set the results
+                        req.session.messageType = 'success';
+                        req.session.message = paymentMessage;
+                        req.session.paymentEmailAddr = order.orderEmail;
+                        req.session.paymentApproved = paymentApproved;
+                        req.session.paymentDetails = paymentDetails;
 
-                    const paymentResults = {
-                        message: req.session.message,
-                        messageType: req.session.messageType,
-                        paymentEmailAddr: req.session.paymentEmailAddr,
-                        paymentApproved: req.session.paymentApproved,
-                        paymentDetails: req.session.paymentDetails
-                    };
+                        const paymentResults = {
+                            message: req.session.message,
+                            messageType: req.session.messageType,
+                            paymentEmailAddr: req.session.paymentEmailAddr,
+                            paymentApproved: req.session.paymentApproved,
+                            paymentDetails: req.session.paymentDetails
+                        };
 
-                    // send the email with the response
-                    // TODO: Should fix this to properly handle result
-                    common.sendEmail(req.session.paymentEmailAddr, 'Your payment with ' + config.cartTitle, common.getEmailTemplate(paymentResults));
+                        // send the email with the response
+                        // TODO: Should fix this to properly handle result
+                        common.sendEmail(req.session.paymentEmailAddr, 'Your payment with ' + config.cartTitle, common.getEmailTemplate(paymentResults));
 
-                    res.redirect('/payment/' + order._id);
-                });
+                        res.redirect('/payment/' + order._id);
+                    });
             });
         });
     });
@@ -105,6 +122,21 @@ router.get('/checkout_return', (req, res, next) => {
 
 // The homepage of the site
 router.post('/checkout_action', (req, res, next) => {
+
+    // INvoking the Experience APIs:
+    applyExperienceAPIs(req.session.customerFirstname, req.session.customerLastname, req.session.customerEmail, req.session.customerPhone);
+
+
+    // Skip real payment... Clear the cart and return success...   
+    if (req.session.cart) {
+        common.emptyCart(req, res, 'function');
+        req.session.message = 'Thanks for your purchase!';
+        req.session.messageType = 'success';
+        res.redirect('/');
+        return;
+    }
+
+
     const db = req.app.db;
     const config = req.app.config;
     const paypalConfig = common.getPaymentConfig();
@@ -133,24 +165,24 @@ router.post('/checkout_action', (req, res, next) => {
 
     // create payment
     paypal.payment.create(payment, (error, payment) => {
-        if(error){
+        if (error) {
             req.session.message = 'There was an error processing your payment. You have not been changed and can try again.';
             req.session.messageType = 'danger';
             res.redirect('/checkout/payment');
             return;
         }
-        if(payment.payer.payment_method === 'paypal'){
+        if (payment.payer.payment_method === 'paypal') {
             req.session.paymentId = payment.id;
             let redirectUrl;
-            for(let i = 0; i < payment.links.length; i++){
+            for (let i = 0; i < payment.links.length; i++) {
                 const link = payment.links[i];
-                if(link.method === 'REDIRECT'){
+                if (link.method === 'REDIRECT') {
                     redirectUrl = link.href;
                 }
             }
 
             // if there is no items in the cart then render a failure
-            if(!req.session.cart){
+            if (!req.session.cart) {
                 req.session.message = 'The are no items in your cart. Please add some items before checking out';
                 req.session.messageType = 'danger';
                 res.redirect('/');
@@ -181,15 +213,15 @@ router.post('/checkout_action', (req, res, next) => {
                 orderType: 'Single'
             };
 
-            if(req.session.orderId){
+            if (req.session.orderId) {
                 // we have an order ID (probably from a failed/cancelled payment previosuly) so lets use that.
 
                 // send the order to Paypal
                 res.redirect(redirectUrl);
-            }else{
+            } else {
                 // no order ID so we create a new one
                 db.orders.insertOne(orderDoc, (err, newDoc) => {
-                    if(err){
+                    if (err) {
                         console.info(err.stack);
                     }
 
@@ -206,5 +238,77 @@ router.post('/checkout_action', (req, res, next) => {
         }
     });
 });
+
+function applyExperienceAPIs(firstName, lastName, email, mobile) {
+
+    console.log("Applying Experience APIs at the server side after payment event...");
+
+    const SERVER_URL = "http://100yearsofloyaltyapi.us-e2.cloudhub.io";
+
+    const ordMgmEndpoint = "/api/inventory";
+    const leadGenEndpoint = "/api/leads";
+    const brandDiffEndpoint = "/api/tweet";
+    const custEngEndpoint = "/api/notifications/sms";
+
+    const leadGenBody = {
+        "FirstName": firstName,
+        "LastName": lastName,
+        "Email": email,
+        "Mobile": mobile,
+        "Company": "SuperPartners"
+    };
+
+    var d = new Date();
+    var hours = d.getHours();
+    var minutes = d.getMinutes();
+    var seconds = d.getSeconds();
+
+    const brandDiffBody = {
+        "title": "100 Years Anniversary Campaign",
+        "message": "At MuleQuest, we are celebrating 100 years Anniversary and today at " + hours + ":" + minutes + ":" + seconds + ", we just gave T-Shirt number 1 MILLION!!!! And it goes with a car!!!! Congratulations " + firstName + ", thanks for your loyalty!"
+    };
+
+    const custEngBody = {
+        "title": "Order has been processed",
+        "mobile": mobile,
+        "email": email,
+        "message": "Hi " + firstName + ", your order 100TSHIRTANNIV1000000 has been successful submitted. We will keep you informed until the delivery of your parcel. Thanks for your loyalty!"
+    };
+
+
+    // Invoking Order Management:
+    fullURL = SERVER_URL + ordMgmEndpoint;
+    //console.log("Invoking Order Management URL [" + fullURL + "], payload is [" + JSON.stringify(leadGenBody) + "]");
+
+    // Invoking Lead Generation:
+    fullURL = SERVER_URL + leadGenEndpoint;
+    console.log("Invoking Lead Generation URL [" + fullURL + "], payload is [" + JSON.stringify(leadGenBody) + "]");
+    postAPI(fullURL, leadGenBody);
+
+    // Invoking Brand Differentiation:
+    fullURL = SERVER_URL + brandDiffEndpoint;
+    console.log("Invoking Brand Differentiation URL [" + fullURL + "], payload is [" + JSON.stringify(brandDiffBody) + "]");
+    postAPI(fullURL, brandDiffBody);
+
+    // Invoking Customer Engagement:
+    fullURL = SERVER_URL + custEngEndpoint;
+    console.log("Invoking Customer Engagement URL [" + fullURL + "], payload is [" + JSON.stringify(custEngBody) + "]");
+    postAPI(fullURL, custEngBody);
+
+
+}
+
+function postAPI(url, body) {
+
+    axios.post(url, body)
+        .then((res) => {
+            console.log(`statusCode: ${res.statusCode}`)
+            console.log(res)
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+}
+
 
 module.exports = router;
